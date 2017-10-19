43f9ec47e353487fbae3f99caafa668eb9916270
Aidan Wiseman <365115@edu.cwa.ac.uk>
2017-10-19 12:47:33 +0100

    SQLCon

---
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/DbFactory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data;
+
+namespace SoftwareEngineeringAssignment
+{
+    public class DbFactory
+    {
+        private static DbConection m_instance = null;
+
+        Dictionary<string, string> m_properties;
+        private static string propfile = "properties-mysql.dat";
+
+        private DbFactory()
+        {
+            m_properties = new Dictionary<string, string>();
+        }
+        public static DbConection instance()
+        {
+            if (null == m_instance)
+            {
+                DbFactory factory = new DbFactory();
+                m_instance = factory.getConection();
+            }
+            return m_instance;
+        }
+        private DbConection getConection()
+        {
+            DbConection connection = null;
+
+            try
+            {
+                m_properties = getProperties();
+                string provider = m_properties["Provider"];
+                if (provider.Equals("MySQL"))
+                    connection = new SQLCon(m_properties);
+                else
+                {
+                    // should throw unsupport exception here
+                    throw new DBException("Not supported provider '" + provider + "'");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Debug.WriteLine("Error file not found" + e.Message);
+                connection = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Property file parsing exception thrown : " + e.Message);
+                connection = null;
+                throw e;
+            }
+            return connection;
+        }
+
+        private Dictionary<string, string> getProperties()
+        {
+            string fileData = "";
+            using (StreamReader sr = new StreamReader(propfile))
+            {
+                fileData = sr.ReadToEnd().Replace("\r", "");
+            }
+            Dictionary<string, string> properties = new Dictionary<string, string>();
+            string[] kvp;
+            string[] records = fileData.Split("\n".ToCharArray());
+            foreach (string record in records)
+            {
+                kvp = record.Split("=".ToCharArray());
+                properties.Add(kvp[0], kvp[1]);
+            }
+            return properties;
+        }
+    }
+    class DBException : System.Exception
+    {
+        public DBException(string message) : base(message) { }
+    }
+
+    public interface DbConection
+    {
+        bool OpenConnection();
+
+        bool CloseConnection();
+
+        
+        //Insert statement
+        void Insert(); 
+        //Update statement
+        void Update();
+        //Delete statement
+        void Delete();
+        
+
+
+        //Select statement
+        DbDataReader Select(String query);
+        
+        //Count statement
+        int Count();
+        void Backup();
+        void Restore();
+       
+        
+
+        DataSet getDataSet(string sqlStatement);
+
+    }
+}
---
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/SQLCon.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data;
+
+namespace SoftwareEngineeringAssignment
+{
+    class SQLCon : DbConection
+    {
+        Dictionary<string, string> m_properties;
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public SQLCon(Dictionary<string, string> properties)
+        {
+            m_properties = properties;
+            initialize();
+        }
+
+
+        private void initialize()
+        {
+            server = m_properties["Server"];
+            database = m_properties["Database"];
+            uid = m_properties["User"];
+
+            setConection();
+        }
+
+        private void setConection()
+        {
+            string connectionString;
+            connectionString = "server=" + server + ";" + "database=" +
+            database + ";" + "uid=" + uid + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public void Backup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CloseConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet getDataSet(string sqlStatement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Restore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbDataReader Select(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
