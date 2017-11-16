1d3b1ed8fad1ad95fd77b803c153c6bf406e0aaf
Aidan Wiseman <365115@edu.cwa.ac.uk>
2017-11-16 16:19:45 +0000

    Started Timetable found bug with shifts

--- SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/Shift.cs
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/Shift.cs
@@ -8,11 +8,9 @@ namespace SoftwareEngineeringAssignment
 {
     public class Shift
     {
-        int m_ShiftID;
-        DateTime m_StartTime, m_EndTime;
+        private int m_StaffID, m_ShiftID;
 
+        public int StaffID { get { return m_StaffID; } set { m_StaffID = value; } }
         public int ShiftID { get { return m_ShiftID; } set { m_ShiftID = value; } }
-        public DateTime StartTime { get { return m_StartTime; } set { m_StartTime = value; } }
-        public DateTime EndTime { get { return m_EndTime; } set { m_EndTime = value; } }
     }
 }
---
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/frmViewTimetable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareEngineeringAssignment
+{
+    public partial class frmViewTimetable : Form
+    {
+        Staff m_currentMember;
+        BackgroundWorker worker = new BackgroundWorker();
+        List<TimeTable> shift = new List<TimeTable>();
+        List<Shift> timetable = new List<Shift>();
+
+        public frmViewTimetable(Staff s)
+        {
+            m_currentMember = s;
+            InitializeComponent();
+
+
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+            worker.WorkerSupportsCancellation = true;
+
+            worker.RunWorkerAsync();
+
+            /*lsvTimeTable.View = View.Details;
+            lsvTimeTable.Columns.Add("Monday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Tuesday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Wednesday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Thursday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Friday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Saturday", -2, HorizontalAlignment.Left);
+            lsvTimeTable.Columns.Add("Sunday", -2, HorizontalAlignment.Left);*/
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            foreach(TimeTable s in shift)
+            {
+                lsvTimeTable.Items.Add(s.StartTime + " - " + s.EndTime);
+            }
+            
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BusinessLayer ml = BusinessLayer.Instance();
+
+            timetable = ml.GetTimeTable(m_currentMember.StaffID);
+
+            foreach(Shift s in timetable)
+            {
+                shift.Add(ml.GetShift(s.ShiftID));
+            }
+        }
+
+        private void ResizeListViewColumns(ListView lv)
+        {
+            foreach (ColumnHeader column in lv.Columns)
+            {
+                column.Width = -2;
+            }
+        }
+    }
+}
