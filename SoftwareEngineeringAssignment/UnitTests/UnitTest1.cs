09956256b2fcb62eaa6792ed196c7a59598541d6
Aidan Wiseman <365115@edu.cwa.ac.uk>
2017-11-28 11:55:25 +0000

    Unit test updates

--- SoftwareEngineeringAssignment/UnitTests/UnitTest1.cs
+++ SoftwareEngineeringAssignment/UnitTests/UnitTest1.cs
@@ -15,11 +15,34 @@ namespace UnitTests
 
             timetable.StartTime = t;
             timetable.EndTime = t.AddHours(9);
-            timetable.ShiftID = 412
+            timetable.ShiftID = 412;
 
             Assert.AreEqual(new DateTime(2017, 1, 4), timetable.StartTime);
             Assert.AreEqual(new DateTime(2017, 1, 4, 9, 0, 0), timetable.EndTime);
             Assert.AreEqual(412, timetable.ShiftID);
+
+            Staff staff = new Staff();
+
+            staff.AuthLevel = (int) Staff.AuthenticationLevel.GP;
+            staff.DoB = new DateTime(2017, 4, 6);
+            staff.FName = "1stName";
+            staff.LName = "2ndName";
+            staff.Password = "password";
+            staff.StaffID = 243;
+            staff.UserName = "Username";
+
+            Assert.AreEqual((int)Staff.AuthenticationLevel.GP, staff.AuthLevel);
+            Assert.AreEqual(new DateTime(2017, 4, 6), staff.DoB);
+            Assert.AreEqual("1stName", staff.FName);
+            Assert.AreEqual("2ndName", staff.LName);
+            Assert.AreEqual("password", staff.Password);
+            Assert.AreEqual(243, staff.StaffID);
+            Assert.AreEqual("Username", staff.UserName);
+
+            BusinessLayer ml = BusinessLayer.Instance();
+            DbConection db = DbFactory.instance();
+            Assert.IsTrue(db.OpenConnection());
+            Assert.IsTrue(db.CloseConnection());            
         }
     }
 }
