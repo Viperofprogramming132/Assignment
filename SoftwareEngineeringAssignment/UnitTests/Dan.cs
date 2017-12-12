203623ea5a7f3bb14842d6afb2ccc09d6b3124f5
Daniel Garnham <401019@edu.cwa.ac.uk>
2017-12-12 09:53:43 +0000

    added Dan Unit tests

---
+++ SoftwareEngineeringAssignment/UnitTests/Dan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareEngineeringAssignment;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Dan
+    {
+        [TestMethod]
+        public void TestBusinessLayerEditPerscription()
+        {
+            BusinessLayer ml = BusinessLayer.Instance();
+            bool success = ml.EditPerscription(5, 5, DateTime.Now, DateTime.Now, "Test Edit Perscription");
+            Assert.AreEqual(true, success);
+        }
+
+        [TestMethod]
+        public void TestBusinessLayerAddPerscription()
+        {
+            BusinessLayer ml = BusinessLayer.Instance();
+            bool success = ml.AddPerscription(5, 5, DateTime.Now, DateTime.Now, "Test add perscription");
+            Assert.AreEqual(true, success);
+        }
+
+        [TestMethod]
+        public void TestBusinessLayerAddStaff()
+        {
+            BusinessLayer ml = BusinessLayer.Instance();
+            bool success = ml.AddStaff("Felix", "McGrath", "FMcGrath", "password", 3, 3, DateTime.Now);
+            Assert.AreEqual(true, success);
+        }
+
+
+
+
+    }
+}
